874e51ee48e4ea356b376d1dd0c2f17453e3dc6e
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-23 11:31:40 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  iteration = 0
+  while iteration < 4
+  yield
+  iteration += 1
+else
+  collection
+end
+end
+
+my_each(collection) do |i|
+  puts i
 end
